039fba6979c7062f88969bd5bde972c4a7286d6a
Eli Evans <eli.evans@faithlife.com>
2020-08-06 21:26:52 -0700

    Update supdata-best-guess.js

--- BibleBooksInteractive/Tools/supdata-best-guess.js
+++ BibleBooksInteractive/Tools/supdata-best-guess.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const Util = require('../../Util/Util.js');
-const Counter = require('../../Util/CounterV1.js');
+const Counter = require('../../Util/Counter.js');
 const Bible = require('../../Util/Bible.js');
 const DatatypeReference = require('../../Util/DatatypeReference.js');
 
@@ -16,34 +16,61 @@ let json = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
 // littype-ot.json
 
 let counter = new Counter();
+counter.pathSeparator = '\t';
 
 json.data.forEach( r => {
 	if (!/^label/.test(r.xref)) {
 		let countable = new DatatypeReference(r.xref).render();
 
 		if (countable) {
-			var book = `${Bible.getBookNumberFromRef(r.ref)}\t${Bible.getBookNameFromRef(r.ref)}`;
+			var booknum = Bible.getBookNumberFromRef(r.ref);
+			var bookname = Bible.getBookNameFromRef(r.ref);
 			var verses = Bible.countVerses(r.ref);
-			verses = verses === NaN ? 15 : verses;
 
-			if (book) { 
-				counter.count(book, countable, verses);
+			verses = verses === NaN ? 15 : verses; // *hack, sputter, cough*
+
+			if (booknum) { 
+				counter.count(booknum, bookname, countable, verses);
 			}
 		}
 	}
 });
 
-// headings = counter.uniqueItemNames();
-headings = [];
+headings = counter.names();
+// headings = [];
 
-console.log( (['Book Number\tTitle', 'Best Guess'].concat(headings)).join('\t') );
+let booksdata = [];
 
-counter.listNames.sort((a,b) => parseInt(a) - parseInt(b)).forEach( listName => {
-	let list = counter.list(listName);
-	let best = counter.dominant(listName, 1.2);
+// console.log( (['Book Number\tTitle', 'Best Guess'].concat(headings)).join('\t') );
 
-	let bestval = best ? best.name : list.items[0].name;
-	let out = [list.name, bestval].concat( headings.map( r => counter.value(list.name, r) ) );
-	console.log(out.join('\t'))
+counter.paths().forEach( pathName => {
+	let out = [pathName, counter.dominant(pathName).name || '??']; // dominant = best guess
+	
+	let parts = pathName.split(counter.pathSeparator);
+	let booknum = parseInt(parts[0]);
+	let bookname = parts[1];
+	let book = Bible.getBookByNumber(booknum);
+	let bookdata = {name: bookname, number: booknum, abbr: book.abbr, data: []} ;
+
+	let analysis = counter.analyze(pathName);
+	let store = {};
+	analysis.items.forEach( item => {
+		store[item.name] = item;
+	});
+
+	headings.forEach(key => {
+		let value = counter.value([pathName, key]);
+		out.push(value);
+
+		if (value) {
+			// bookdata.data.push({name: key, value: value});
+			bookdata.data.push( store[key] );
+		}
+	});
+
+	booksdata.push( bookdata );
+	// console.log(out.join('\t'));
 });
+
+console.log(JSON.stringify(booksdata, null, 4));
 
