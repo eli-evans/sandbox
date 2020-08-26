00968a87dcf23827c80e9d44cca72dff40f494aa
Eli Evans <eli.evans@faithlife.com>
2020-08-25 23:43:56 -0700

    Update Composition.js
    Alphabetical. Add Articulation.

--- Music/Composition.js
+++ Music/Composition.js
@@ -1,15 +1,17 @@
-const Score = require('./Score.js');
-const Voice = require('./Voice.js');
-const Key = require('./Key.js');
-const Scale = require('./Scale.js');
-const Row = require('./Row.js');
-const Sequence = require('./Sequence.js');
-const Note = require('./Note.js');
-const Pitch = require('./Pitch.js');
-const Dynamics = require('./Dynamics.js');
+const {Event, Note, Rest} = require('./Note.js');
+const Articulation = require('./Articulation.js');
 const Duration = require('./Duration.js');
+const Dynamics = require('./Dynamics.js');
+const Key = require('./Key.js');
+const Pitch = require('./Pitch.js');
+const Row = require('./Row.js');
+const Scale = require('./Scale.js');
+const Score = require('./Score.js');
+const Sequence = require('./Sequence.js');
 const Util = require('./Util.js');
+const Voice = require('./Voice.js');
 
 module.exports = {
-	Score, Voice, Key, Scale, 
-	Row, Sequence, Note, Pitch, Dynamics, Duration, Util};
+	Articulation, Duration, Dynamics, Event, Key, Note, Pitch, 
+	Rest, Row, Scale, Score, Sequence, Util, Voice, 
+};
