5100dd65851832967d0613ff92894af51176d61e
elievans <38299390+eli-evans@users.noreply.github.com>
2021-07-27 11:45:19 -0700

    Update csv2matrix.js

--- MorphCharts/tools/csv2matrix.js
+++ MorphCharts/tools/csv2matrix.js
@@ -1,15 +1,17 @@
 const fs = require('fs');
 
-if (process.argv.length !== 4) {
-	console.log("USAGE: node csv2matrix.js <csvfile> <output>")
+if (process.argv.length !== 6) {
+	console.log("USAGE: node csv2matrix.js <csvfile> <output> <title> <for>")
 	process.exit();
 }
 let inpath = process.argv[2];
 let outpath = process.argv[3];
+let title = process.argv[4];
+let isFor = process.argv[5];	
 
 let infile = fs.readFileSync(inpath).toString();
 
-let out = [`{\n\t"title": "",\n\t"for": "",\n\t"grid": [`];
+let out = [`{\n\t"title": "${title}",\n\t"for": "${isFor}",\n\t"grid": [`];
 
 out.push(
 	infile.split(/[\r\n]+/)
